3b77e32cfdea38da07e0fe07dd19c55bc587e307
layafakher <62253882+layafakher@users.noreply.github.com>
2020-04-09 10:22:22 +0430

    Update Main.java

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -54,7 +54,7 @@ public class Main {
         foreignTours.add(foreignTour);foreignTours.add(foreignTour1);
         Scanner scanner1=new Scanner(System.in);
         while (true){
-            System.out.println("Press 1 to see tour leaser menu:");
+            System.out.println("Press 1 to see tour leader menu:");
             System.out.println("Press 2 to see tours menu:");
             System.out.println("Press 3 to see Map:");
             System.out.println("Press 4 to see regions menu:");
